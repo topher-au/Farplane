7504a598c8232bff82bb9b5ad4f4ecce92a7735f
Topher Sheridan <topher@xobanimot.com>
2016-06-03 17:53:22 +1000

    Added standard item editor

--- FFX/EditorPanels/Items/ItemsPanel.xaml.cs
+++ FFX/EditorPanels/Items/ItemsPanel.xaml.cs
@@ -23,20 +23,90 @@ namespace Farplane.FFX.EditorPanels.Items
     public partial class ItemsPanel : UserControl
     {
         private ButtonGrid _itemButtons = new ButtonGrid(2,112);
+        private Item[] _currentItems;
+        private int _editingItem = -1;
+
+        private static ComboBox _comboItemList = new ComboBox() {ItemsSource = Item.Items.Select(item => item.Name) };
+        private static TextBox _textItemCount = new TextBox();
+        private static StackPanel _panelEditItem = new StackPanel()
+        {
+            Orientation = Orientation.Horizontal,
+                Children =
+                {
+                    _comboItemList,
+                    _textItemCount
+                }
+        };
 
         public ItemsPanel()
         {
             InitializeComponent();
             TabItems.Content = _itemButtons;
+            _itemButtons.ButtonClicked += ItemButtonsOnButtonClicked;
+            _comboItemList.KeyDown += ItemEditor_KeyDown;
+            _textItemCount.KeyDown += ItemEditor_KeyDown;
+
+            
+        }
+
+        private void SaveItem(int buttonIndex, int itemID, byte itemCount)
+        {
+            Item.WriteItem(buttonIndex, itemID, itemCount);
+        }
+        
+        private void ItemButtonsOnButtonClicked(int buttonIndex)
+        {
+            if (_editingItem == buttonIndex) return;
+
+            Refresh();
+
+            var clickedItem = _currentItems[buttonIndex];
+            var baseItem = Item.Items.First(item => item.ID == clickedItem.ID);
+
+            var itemIndex = Item.Items.ToList().IndexOf(baseItem);
+
+            _comboItemList.SelectedIndex = itemIndex;
+            _comboItemList.KeyDown += ItemEditor_KeyDown;
+
+            _textItemCount.Text = clickedItem.Count.ToString();
+
+            _itemButtons.SetContent(buttonIndex, _panelEditItem);
+            _editingItem = buttonIndex;
+
+            _textItemCount.SelectionStart = 0;
+            _textItemCount.SelectionLength = _textItemCount.Text.Length;
+
+            _textItemCount.Focus();
+        }
+
+        private void ItemEditor_KeyDown(object sender, KeyEventArgs keyEventArgs)
+        {
+            if (keyEventArgs.Key != Key.Enter && keyEventArgs.Key != Key.Escape) return;
+
+            switch (keyEventArgs.Key)
+            {
+                case Key.Enter:
+                    var itemIndex = _comboItemList.SelectedIndex;
+                    var itemCount = byte.Parse(_textItemCount.Text);
+                    if (itemCount == 0) itemIndex = 0;
+                    if (itemIndex == 0) itemCount = 0;
+                    Item.WriteItem(_editingItem, Item.Items[itemIndex].ID, itemCount);
+                    Refresh();
+                    break;
+                case Key.Escape:
+                    Refresh();
+                    break;
+            }
         }
 
         public void Refresh()
         {
-            var items = Item.ReadItems();
+            _editingItem = -1;
+            _currentItems = Item.ReadItems();
             
-            for (int i = 0; i < items.Length; i++)
+            for (int i = 0; i < _currentItems.Length; i++)
             {
-                if (items[i].ID == 0xFF)
+                if (_currentItems[i].ID == 0xFF)
                 {
                     // Empty slot
                     _itemButtons.SetContent(i, "< EMPTY >");
@@ -44,9 +114,8 @@ namespace Farplane.FFX.EditorPanels.Items
                 else
                 {
                     // Show item name and count
-                    _itemButtons.SetContent(i, items[i].Name + " x" + items[i].Count);
+                    _itemButtons.SetContent(i, _currentItems[i].Name + " x" + _currentItems[i].Count);
                 }
-                
             }
         }
     }
