8665604df78cc7e4937528f142c9b05fe32c7311
Topher Sheridan <topher@xobanimot.com>
2016-06-03 17:52:29 +1000

    Base64 serialization for data import/export

---
+++ Common/DataTextSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farplane.Common
+{
+    public static class DataTextSerializer
+    {
+        public static string Serialize(byte[] dataToSerialize)
+        {
+            var base64 = Convert.ToBase64String(dataToSerialize);
+            return base64;
+        }
+
+        public static byte[] Deserialize(string stringToDeserialize)
+        {
+            var data = Convert.FromBase64String(stringToDeserialize);
+            return data;
+        }
+    }
+}
