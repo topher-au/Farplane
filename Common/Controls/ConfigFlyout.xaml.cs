a598ad81e096445448f670bcf2de845b9a16bb60
Topher <lordramza@gmail.com>
2018-04-21 22:44:35 +1000

    Removed unused options from config panel

--- Common/Controls/ConfigFlyout.xaml.cs
+++ Common/Controls/ConfigFlyout.xaml.cs
@@ -42,11 +42,9 @@ namespace Farplane.Common.Controls
                 ThemeManager.Accents.ToList().IndexOf(currentAccent);
 
             CheckNeverShowUnXWarning.IsChecked = Settings.Default.NeverShowUnXWarning;
-            CheckNeverShowDonation.IsChecked = Settings.Default.NeverShowDonation;
             CheckCloseWithGame.IsChecked = Settings.Default.CloseWithGame;
             CheckShowAllProcesses.IsChecked = Settings.Default.ShowAllProcesses;
-
-            CheckEnableMods.IsChecked = Settings.Default.EnableMods;
+			
 
             canSetTheme = true;
         }
@@ -61,37 +59,11 @@ namespace Farplane.Common.Controls
         private void SettingUpdated(object sender, RoutedEventArgs e)
         {
             if (!canSetTheme) return;
-            Settings.Default.NeverShowDonation = CheckNeverShowDonation.IsChecked.Value;
             Settings.Default.NeverShowUnXWarning = CheckNeverShowUnXWarning.IsChecked.Value;
             Settings.Default.CloseWithGame = CheckCloseWithGame.IsChecked.Value;
             Settings.Default.ShowAllProcesses = CheckShowAllProcesses.IsChecked.Value;
             Settings.Default.AppAccent = (ComboAccent.SelectedItem as Accent).Name;
             Settings.Default.AppTheme = (ComboTheme.SelectedItem as AppTheme).Name;
-            Settings.Default.Save();
-        }
-
-        private void CheckEnableMods_OnChecked(object sender, RoutedEventArgs e)
-        {
-            if (!canSetTheme) return;
-
-            if (CheckEnableMods.IsChecked.Value)
-            {
-                var modWarning =
-                MessageBox.Show(
-                    "Farplane mods run on a powerful scripting engine, and could potentially contain " +
-                    "malicious code or code that may otherwise be harmful to your computer.\n\n" +
-                    "You should only ever install mods from a trusted source.\n\n" +
-                    "Are you sure you want to enable mods to run on this computer?",
-                    "Mod Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-
-                if (modWarning == MessageBoxResult.No)
-                {
-                    CheckEnableMods.IsChecked = false;
-                    return;
-                }
-            }
-
-            Settings.Default.EnableMods = CheckEnableMods.IsChecked.Value;
             Settings.Default.Save();
         }
     }
